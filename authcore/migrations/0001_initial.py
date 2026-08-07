32e65118c05843e587137971662b3e75b01eb1fa
Kouassi Sylvester <sylvesterkouassi9@gmail.com>
2026-08-07 15:10:28 +0100

    feat(employee_registration):Refactor user model: update role choices to uppercase and add status field with predefined choices

--- authcore/migrations/0001_initial.py
+++ authcore/migrations/0001_initial.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 ('email', models.EmailField(max_length=254, unique=True)),
                 ('first_name', models.CharField(max_length=30)),
                 ('last_name', models.CharField(max_length=30)),
-                ('role', models.CharField(choices=[('employee', 'Employee'), ('admin', 'Admin')], default='employee', max_length=20)),
+                ('role', models.CharField(choices=[('ADMIN', 'Admin'), ('EMPLOYEE', 'Employee')], default='EMPLOYEE', max_length=20)),
                 ('is_staff', models.BooleanField(default=False)),
                 ('is_superuser', models.BooleanField(default=False)),
                 ('is_active', models.BooleanField(default=True)),
--- authcore/models/user.py
+++ authcore/models/user.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.db import models
 import uuid
 from datetime import datetime
+from common.constants.roles import Roles
+from common.constants.status import Status
 
 
 class CustomUserManager(BaseUserManager):
@@ -25,21 +27,17 @@ class CustomUserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    ROLE_CHOICES = [
-        ('employee', 'Employee'),
-        ('admin', 'Admin'),
-    ]
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     emp_id = models.CharField(max_length=20, unique=True, editable=False)
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30) 
     last_name = models.CharField(max_length=30) 
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='employee')
+    role = models.CharField(max_length=20, choices=Roles.CHOICES, default='Roles.EMPLOYEE')
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_verified = models.BooleanField(default=False)
+    status = models.CharField(max_length=20,choices=Status.CHOICES, default=Status.ACTIVE)
     deleted_at = models.DateTimeField(null=True, blank=True)
 
     USERNAME_FIELD = 'email'
@@ -48,34 +46,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return f"{self.email} ({self.emp_id})"
-
-    def save(self, *args, **kwargs):
-        if not self.emp_id:
-            self.emp_id = self.generate_emp_id()
-        super().save(*args, **kwargs)
-
-    @staticmethod
-    def generate_emp_id():
-        """
-        Generates a unique EMP ID in the format EMP-YEAR-NNN
-        Uses a DB transaction and row-level lock to avoid race conditions.
-        """
-        from django.utils import timezone
-        year = timezone.now().year
-
-        from django.db.models import Max
-        from django.db import transaction
-
-        with transaction.atomic():
-            # Lock table for safe increment
-            last_id = User.objects.filter(emp_id__startswith=f"EMP-{year}-") \
-                                    .select_for_update() \
-                                    .aggregate(Max('emp_id'))['emp_id__max']
-            if last_id:
-                last_num = int(last_id.split('-')[-1])
-                new_num = last_num + 1
-            else:
-                new_num = 1
-            return f"EMP-{year}-{str(new_num).zfill(3)}"
-
-
